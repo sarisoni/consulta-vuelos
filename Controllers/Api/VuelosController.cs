4222a165d582fda9664c93f06d5d8950866ad0ed
sarisoni <sariameli.sonilopez.gen23@cbtis105.edu.mx>
2025-05-14 09:58:19 -0600

    parte 3 :)

--- Controllers/Api/VuelosController.cs
+++ Controllers/Api/VuelosController.cs
@@ -39,46 +39,55 @@ public class VuelosController : ControllerBase {
         return Ok(lista);
     }
 
-     [HttpGet("listar-vuelos")]
-    public IActionResult ListarVuelos(string? estatus, string? origen, string? destino,
-    string? fechaInicial, string? fechaFinal){
-        
-        var client = new MongoClient(CadenasConexion.MONGO_DB);
-        var db = client.GetDatabase("Aeropuerto");
-        var collection = db.GetCollection<Vuelo>("Vuelos");
+  [HttpGet("listar-vuelos")]
+  public IActionResult ListarVuelos(string? estatus, string? origen, string? destino, 
+  string? fechaInicial, string? fechaFinal)
+{
+    var client = new MongoClient(CadenasConexion.MONGO_DB);
+    var db = client.GetDatabase("Aeropuerto");
+    var collection = db.GetCollection<Vuelo>("Vuelos");
 
-        List<FilterDefinition<Vuelo>> filters = new   List<FilterDefinition<Vuelo>>();
+    List<FilterDefinition<Vuelo>> filters = new List<FilterDefinition<Vuelo>>();
 
-        if(!string.IsNullOrWhiteSpace(estatus)){
-         var filterEstatus = Builders<Vuelo>.Filter.Eq(x => x.EstatusVuelo, estatus);
-         filters.Add(filterEstatus);
-        }
-         if(!string.IsNullOrWhiteSpace(origen)){
-         var filterOrigen = Builders<Vuelo>.Filter.Eq(x => x.CiudadOrigen, origen);
-         filters.Add(filterOrigen);
-        }
+    if (!string.IsNullOrWhiteSpace(estatus)){
+        var filterEstatus = Builders<Vuelo>.Filter.Eq(x => x.EstatusVuelo, estatus);
+        filters.Add(filterEstatus);
+    }
 
-         if(!string.IsNullOrWhiteSpace(destino)){
-         var filterDestino = Builders<Vuelo>.Filter.Eq(x => x.CiudadDestino, destino);
-         filters.Add(filterDestino);
-        }
-     
-        if(!string.IsNullOrWhiteSpace(fechaInicial)){
-         if(DateTime.TryParse(fechaInicial, out DateTime fecha)){
-         var filterfechaInicial = Builders<Vuelo>.Filter.Eq(x => x.FechaSalida, fechga);
-         filters.Add(filterDestino);
-        }
-     
+    if (!string.IsNullOrWhiteSpace(origen)){
+        var filterOrigen = Builders<Vuelo>.Filter.Eq(x => x.CiudadOrigen, origen);
+        filters.Add(filterOrigen);
+    }
 
-        List<Vuelo> vuelos;
-        if (filters.Count > 0){
-             var filter = Builders<Vuelo>.Filter.And(filters);
-             vuelos = collection.Find(filter).ToList();
+    if (!string.IsNullOrWhiteSpace(destino)){
+        var filterDestino = Builders<Vuelo>.Filter.Eq(x => x.CiudadDestino, destino);
+        filters.Add(filterDestino);
+    }
+
+    if (!string.IsNullOrWhiteSpace(fechaInicial)){
+        if (DateTime.TryParse(fechaInicial, out DateTime fechaInicio)){
+            var filtroFechaIni = Builders<Vuelo>.Filter.Gte(x => x.FechaHoraSalida, fechaInicio);
+            filters.Add(filtroFechaIni);
         }
-        else{
-             vuelos = collection.Find(FilterDefinition<Vuelo>.Empty).ToList();
+    }
+
+    if (!string.IsNullOrWhiteSpace(fechaFinal)){
+        if (DateTime.TryParse(fechaFinal, out DateTime fechaFin)){
+            var filtroFechaFin = Builders<Vuelo>.Filter.Lte(x => x.FechaHoraSalida,
+                new DateTime(fechaFin.Year, fechaFin.Month, fechaFin.Day, 23, 59, 59));
+            filters.Add(filtroFechaFin);
         }
+    }
+
+    List<Vuelo> vuelos;
+    if (filters.Count > 0){
+        var filter = Builders<Vuelo>.Filter.And(filters);
+        vuelos = collection.Find(filter).ToList();
+    }
+    else
+    {
+        vuelos = collection.Find(FilterDefinition<Vuelo>.Empty).ToList();
+    }
 
     return Ok(vuelos);
-}
-}
+}}
